ef8d3882a36673ae847235f2e38789df2f2a3b56
Abhishek Kumar <abhi993575@gmail.com>
2025-07-09 02:21:08 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,19 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+const allowedOrigins = process.env.FRONTEND_ENDPOINT;
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
