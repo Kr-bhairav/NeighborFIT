0095c01b308c957b803966ece8a58cf435344eeb
Bhairav kumar <112298097+Bhairav374@users.noreply.github.com>
2025-09-18 06:31:36 +0530

    some changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,17 +3,17 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white py-6">
       <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
         <div className="text-sm">
-          © {new Date().getFullYear()} <span className="font-semibold">Abhishek Kumar</span>. All rights reserved.
+          © {new Date().getFullYear()} <span className="font-semibold">Bhairav Kumar</span>. All rights reserved.
         </div>
         <div className="mt-4 md:mt-0 flex gap-4 text-sm">
           <a
-            href="mailto:abhi993575@gmail.com"
+            href="mailto:kumarbhairav9142@gmail.com"
             className="hover:text-blue-400 transition"
           >
              Email
           </a>
           <a
-            href="https://linkedin.com/in/abhishek-kumar-rathour"
+            href="https://www.linkedin.com/in/bhairav-kumar/"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-blue-400 transition"
@@ -21,7 +21,7 @@ const Footer = () => {
              LinkedIn
           </a>
           <a
-            href="https://github.com/Di-Abhi"
+            href="https://github.com/Kr-bhairav"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-blue-400 transition"
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,7 +3,7 @@ const About = () => (
     <div className="max-w-4xl mx-auto px-4 text-center">
       <h2 className="text-4xl font-bold text-gray-800 mb-4">About NeighborFit</h2>
       <p className="text-gray-700 text-lg leading-relaxed">
-        I’m <span className="text-purple-700 font-semibold">Abhishek Kumar</span>, the creator of <strong>NeighborFit</strong> — a smart tool built to help people like you find neighborhoods that actually fit your life.
+        I’m <span className="text-purple-700 font-semibold">Bhairav Kumar</span>, the creator of <strong>NeighborFit</strong> — a smart tool built to help people like you find neighborhoods that actually fit your life.
         <br /><br />
         Whether it’s safety, affordability, or access to schools and parks, this tool uses real data + smart matching to guide your lifestyle decisions.
       </p>
